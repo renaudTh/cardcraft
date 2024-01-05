f4e50bf875b6ab8d36a63773d97ad2a22852905a
Thomas Renaud <t.renaud@neuf.fr>
2024-01-05 22:20:09 +0100

    feat(rust): add stack unit test

--- craftlib/src/stack.rs
+++ craftlib/src/stack.rs
@@ -81,3 +81,110 @@ impl Stack {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_empty_stack() {
+        let stack = Stack::new_empty();
+        assert!(stack.is_empty());
+    }
+
+    #[test]
+    fn test_add_card_on_top() {
+        let mut stack = Stack::new_empty();
+        let card_added = Card::new(1, 8, true);
+        let card_added_2 = Card::new(2, 10, true);
+
+        let card_ref = Card::new(2, 10, true);
+
+        stack.add_card_on(card_added);
+        stack.add_card_on(card_added_2);
+
+        assert_eq!(stack.is_empty(), false);
+        let first = stack.read_first();
+        assert!(first.is_some_and(|card| card.equal(&card_ref)))
+    }
+
+    #[test]
+    fn test_add_card_under() {
+        let mut stack = Stack::new_empty();
+        let card_added = Card::new(2, 10, true);
+        let card_ref = Card::new(2, 10, true);
+        stack.add_card_under(card_added);
+        assert_eq!(stack.is_empty(), false);
+        assert!(stack.read_last().is_some_and(|card| card.equal(&card_ref)));
+    }
+
+    #[test]
+    fn test_flip_stack() {
+        let mut stack = Stack::new_empty();
+        let card1 = Card::new(0, 5, true);
+        let card1_ref = Card::new(0, 5, true);
+        let card2 = Card::new(1, 9, true);
+        let card2_ref = Card::new(1, 9, true);
+        stack.add_card_on(card1);
+        stack.add_card_on(card2);
+        stack.flip();
+        assert!(stack.read_first().is_some_and(|card| card.equal(&card1_ref)));
+        assert!(stack.read_last().is_some_and(|card| card.equal(&card2_ref)));
+    }
+
+    #[test]
+    fn test_new_complete_deck() {
+        let stack = Stack::new_complete_deck(32, true, true);
+        assert_eq!(stack.is_empty(), false);
+        assert_eq!(stack._deck.len(), 32);
+    }
+
+    #[test]
+    fn test_pop_from_top_to_top() {
+        let mut stack1 = Stack::new_complete_deck(32, true, true);
+        let mut stack2 = Stack::new_empty();
+
+        stack1.pop_from_top_to_top(&mut stack2);
+
+        assert_eq!(stack1.is_empty(), false);
+        assert_eq!(stack2.is_empty(), false);
+        assert_eq!(stack1._deck.len(), 31);
+        assert_eq!(stack2._deck.len(), 1);
+    }
+
+    #[test]
+    fn test_pop_from_top_to_bottom() {
+        let mut stack1 = Stack::new_complete_deck(32, true, true);
+        let mut stack2 = Stack::new_empty();
+
+        stack1.pop_from_top_to_bottom(&mut stack2);
+
+        assert_eq!(stack1.is_empty(), false);
+        assert_eq!(stack2.is_empty(), false);
+        assert_eq!(stack1._deck.len(), 31);
+        assert_eq!(stack2._deck.len(), 1);
+    }
+
+    #[test]
+    fn test_append_on_bottom() {
+        let mut stack1 = Stack::new_complete_deck(32, true, true);
+        let mut stack2 = Stack::new_complete_deck(32, false, false);
+
+        stack1.append_on_bottom(&mut stack2);
+
+        assert_eq!(stack1.is_empty(), false);
+        assert_eq!(stack2.is_empty(), true);
+        assert_eq!(stack1._deck.len(), 64);
+    }
+
+    #[test]
+    fn test_append_on_top() {
+        let mut stack1 = Stack::new_complete_deck(32, true, true);
+        let mut stack2 = Stack::new_complete_deck(32, false, false);
+
+        stack1.append_on_top(&mut stack2);
+
+        assert_eq!(stack1.is_empty(), false);
+        assert_eq!(stack2.is_empty(), true);
+        assert_eq!(stack1._deck.len(), 64);
+    }
+}
