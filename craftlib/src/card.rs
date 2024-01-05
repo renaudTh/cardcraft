b0a585f51988e6a444f4f9bd47d08e65efe23ac5
Thomas Renaud <t.renaud@neuf.fr>
2024-01-05 22:15:59 +0100

    feat(rust): add card unit tests

--- craftlib/src/card.rs
+++ craftlib/src/card.rs
@@ -49,3 +49,60 @@ impl Card {
         self.value() == other.value() && self.family() == other.family()
     }
 }
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_visible_card() {
+        let card = Card::new(2, 5, true);
+        assert_eq!(card.family(), 2);
+        assert_eq!(card.value(), 5);
+        assert_eq!(card.is_visible(), true);
+    }
+    #[test]
+    fn test_new_hidden_card() {
+        let card = Card::new(2, 5, false);
+        assert_eq!(card.family(), 2);
+        assert_eq!(card.value(), 5);
+        assert_eq!(card.is_visible(), false);
+    }
+    #[test]
+    fn test_flip() {
+        let mut card = Card::new(3, 6, true);
+        assert_eq!(card.is_visible(), true);
+        card.flip();
+        assert_eq!(card.is_visible(), false);
+        card.flip();
+        assert_eq!(card.is_visible(), true);
+    }
+
+    #[test]
+    fn test_to_string_visible() {
+        let card = Card::new(0, 8, true);
+        assert_eq!(card.to_string(), "8C".to_string());
+    }
+
+    #[test]
+    fn test_to_string_hidden() {
+        let card = Card::new(3, 11, false);
+        assert_eq!(card.to_string(), "#".to_string());
+    }
+
+    #[test]
+    fn test_is_inf() {
+        let card1 = Card::new(1, 7, true);
+        let card2 = Card::new(0, 9, true);
+        assert!(card1.is_inf(&card2));
+        assert!(!card2.is_inf(&card1));
+    }
+
+    #[test]
+    fn test_equal() {
+        let card1 = Card::new(2, 4, true);
+        let card2 = Card::new(2, 4, true);
+        let card3 = Card::new(1, 4, true);
+        assert!(card1.equal(&card2));
+        assert!(card2.value_equal(&card3));
+    }
+}
